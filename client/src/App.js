20c63219955f772e5489243ffbef167cd83689ff
Fritzhoy <camila.gmarques2@gmail.com>
2022-06-20 22:17:49 -0300

    react project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(){
+ return(
+  <div>
+    Multisig DaP
+  </div>
+
+ );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+React.DOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+ document.getElementById('root')
+);
+
+
