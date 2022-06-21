fbeba995ffb095c671acde9e62d0a5ce79446abf
Fritzhoy <camila.gmarques2@gmail.com>
2022-06-20 21:53:13 -0300

    migration-deploy

---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,9 @@
+//to deploy the contract need to be in the truffle develop
+//remember to use migrate --reset 
+const Wallet = artifacts.require("Wallet");
+
+module.exports = async function(deployer, _network, accounts) {
+  await deployer.deploy(Wallet, [accounts[0],accounts[1],accounts[2]], 2);
+  const wallet = await Wallet.deployed();
+  await web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 10000});
+};
