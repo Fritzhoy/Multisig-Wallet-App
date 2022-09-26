83a4667756090aad54d8a143336216e3f72d3ca4
Camila marques <mila_@MacBook-Pro-3.local>
2022-09-26 15:16:16 -0300

    front app

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { getWeb3, getWallet } from './utils.js';
 import Header from './Header.js';
 import './App.css';
+import NewTransfer from './NewTransfer.js'
+import TransferList from './TransferList.js';
+
 
 function App() {
   const [web3, setWeb3] = useState(undefined);
@@ -9,23 +12,37 @@ function App() {
   const [wallet, setWallet] = useState(undefined);
   const [approvers, setApprovers] = useState([]);
   const [quorum, setQuorum] = useState(undefined);
+  const [transfers, setTransfers] = useState([]);
 
   useEffect(() => {
     const init = async () => {
-      const web3 = getWeb3();
+      const web3 = await getWeb3();
       const accounts = await web3.eth.getAccounts();
       console.log(accounts);
       const wallet = await getWallet(web3);
       const approvers = await wallet.methods.getApprovers().call(); 
       const quorum  = await wallet.methods.quorum().call();
+      const transfers = await wallet.methods.getTransfers().call();
       setWeb3(web3);
       SetAccounts(accounts);
       setWallet(wallet);
       setApprovers(approvers);
       setQuorum(quorum);
+      setTransfers(transfers);
     };
     init();
   }, []);
+  /* 
+  @Dev use #send function when your transaction will modify the data
+  object created to call createTransfer function and approveTransfer  from our smart contract
+  */
+  const createTransfer = transfer => {
+    wallet.methods.createTransfer(transfer.amount, transfer.to).send({ from: accounts[0] })
+  }
+  
+  const approveTransfer = transferId => {
+    wallet.methods.approveTransfer(transferId).send({from: accounts[0]});
+  }
 
   if(
     typeof web3 === 'undefined' 
@@ -40,7 +57,9 @@ function App() {
   return (
     <div className="App">
           Multisig Dapp
-          <Header approvers={approvers} quorum={quorum}></Header>
+          <Header approvers={approvers} quorum={quorum}/>
+          <NewTransfer createTransfer={createTransfer}/>
+          <TransferList transfers={transfers} approveTransfer={approveTransfer}/>
     </div>
   );
 }
--- client/src/utils.js
+++ client/src/utils.js
@@ -3,8 +3,32 @@ import Wallet from './contracts/Wallet.json';
 
 
 //Web3 object that give us connection to the ethereum blockchain (node that we use to deploy our contract)
+
 const getWeb3 = () => {
-  return new Web3('HTTP://localhost:9545');
+  //const web3 = new Web3('http://localhost:8545');
+  //return promise to test if metamask is injected in our code 
+  return new Promise((resolve, reject) => {
+
+    window.addEventListener('load', async () => {
+    if(window.ethereum){
+      //when you instance the Web3 inside we 'call a provider object', window.
+      const web3 = new Web3(window.ethereum);
+      //inside a try object, in case the user reject to grant access to his metamask
+      try {
+        await window.ethereum.enable();
+        resolve(web3);
+      } catch(error) {
+        reject(error)
+      }
+    // if user uses a old version of metamask    
+    } else if(window.web3){
+      resolve(window.web3);
+    } else {
+      reject('Must install Metamask');
+    }
+
+    });
+  });
 }
 
 // contract instance it's a object that is product from web3 that allow to interact with smart contracts
